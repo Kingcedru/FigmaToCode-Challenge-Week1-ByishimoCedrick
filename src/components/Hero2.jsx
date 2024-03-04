477327efc25e6cd861be4394be27ca5ea1bead2b
Kingcedru <byishimocedrick@gmail.com>
2024-03-04 15:39:37 +0200

    hero2 mobile finish

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -1,13 +1,13 @@
-import doctor from "../assets/page2/doctor.png";
+import doctor from "../assets/page2/doc.png";
 import Booking from "./utils/Booking";
-import whatsapp from "../assets/hero/whatsapp.png";
+import whatsapp from "../assets/page2/whatsapp.png";
 export default function Hero2() {
   return (
-    <section className="px-10 md:py-4 md:px-7 mt-20">
+    <section className="">
       <div>
-        <img src={doctor} alt="" />
+        <img src={doctor} alt="docter image" />
       </div>
-      <div className="flex flex-col md:items-start gap-3 md:w-[660px] md:justify-center my-5">
+      <div className="flex px-10  md:px-7 flex-col md:items-start gap-3 md:w-[660px] md:justify-center my-5">
         <h1 className="text-title-500 text-2xl font-poppin font-medium space-x-4 flex justify-start md:justify-start md:w-72">
           About me
         </h1>
@@ -21,11 +21,13 @@ export default function Hero2() {
           lobortis. Vulputate tortor tincidun.
         </p>
       </div>
-      <Booking
-        image={whatsapp}
-        text="Book an appointment"
-        color="bg-[linear-gradient(96deg,#3A8EF6_-10.84%,#6F3AFA_196.74%)] text-white"
-      />
+      <div className="px-10 md:px-7 flex justify-center">
+        <Booking
+          image={whatsapp}
+          text="Book an appointment"
+          color="bg-[linear-gradient(96deg,#3A8EF6_-10.84%,#6F3AFA_196.74%)] text-white"
+        />
+      </div>
     </section>
   );
 }
