4ecd98350fd34117305c96dfe84d9985486cdbd6
Kingcedru <byishimocedrick@gmail.com>
2024-03-06 19:54:26 +0200

    styled hero5

--- src/components/Hero5.jsx
+++ src/components/Hero5.jsx
@@ -5,24 +5,34 @@ import plus from "../assets/page5/med.svg";
 import clock from "../assets/page5/clock.svg";
 export default function Hero5() {
   return (
-    <section className="px-10 md:px-7 pb-10 flex flex-col gap-5">
+    <section className="md:py-8 md:px-20 px-10 pb-10 flex flex-col gap-7 md:gap-16 md:my-20">
       <div className="flex justify-center">
         <h1 className="text-title-500 text-xl font-poppin font-semibold flex md:justify-start text-center md:w-72">
           Book an appointement
         </h1>
       </div>
-      <img src={doctor} alt="doctor with a patient" />
-      <p className="flex text-center font-poppin md:text-5x text-2xl font-bold text-subtitles-950">
-        A dedicated doctor with the core mission to help
-      </p>
-      <p className="flex font-poppin md:text-5x text-xl font-normal text-subtitles-950 text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas egestas
-        viverra.
-      </p>
+      <div className="flex justify-center">
+        <img
+          src={doctor}
+          alt="doctor with a patient"
+          className="md:w-5/6 md:h-6/6 flex justify-center"
+        />
+      </div>
       <div className="flex flex-col gap-5">
+        <p className="flex justify-center font-poppin md:text-5x text-2xl font-bold text-subtitles-950">
+          A dedicated doctor with the core mission to help
+        </p>
+        <p className="flex justify-center text-xl font-light text-paragraphs-500 ">
+          orem ipsum dolor sit amet, consectetur adipiscing elit. Quam proin
+          nibh cursus at sed sagittis amet, sed. Tristique id nibh lobortis nunc
+          elementum. Tellus quam mauris aenean turpis vulputate sodales nullam
+          lobortis. Vulputate tortor tincidun.
+        </p>
+      </div>
+      <div className="flex flex-col gap-5 md:flex-row md:justify-center md:gap-20">
         <Box image={clock} title={"+15 years of experience"} />
         <Box image={check} title={"Urgent 24 hour service"} />
-        <div className="w-6/6 flex justify-center">
+        <div className="w-6/6  flex justify-center">
           <Box image={plus} title={"High quality care"} />
         </div>
       </div>
--- src/components/utils/Box.jsx
+++ src/components/utils/Box.jsx
@@ -1,11 +1,10 @@
 export default function Box(prop) {
   return (
-    <div className="flex bg-[linear-gradient(145deg,#1678F2_3.73%,#65A8FB_132.69%)] items-center gap-3 rounded-xl py-2 px-4">
+    <div className="flex bg-[linear-gradient(145deg,#1678F2_3.73%,#65A8FB_132.69%)] items-center shadow-xl gap-3 rounded-xl py-2 px-4">
       <img src={prop.image} alt="" className="" />
-        <h3 className="text-white font-poppin font-semibold  flex text-center">
-          {prop.title}
-        </h3>
-
+      <h3 className="text-white font-poppin font-semibold  flex text-center">
+        {prop.title}
+      </h3>
     </div>
   );
 }
